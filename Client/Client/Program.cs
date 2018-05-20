8451acbb47bf0d0ac30fc5eb3449946af26a43ad
BuildTools <unconfigured@null.spigotmc.org>
2018-05-20 21:58:38 +0900

    Add Method "Game " which is when gameWindow is opening, gameWindow is clear with Red color and display,

---
+++ Client/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+
+namespace Client
+{
+    class Program
+    {
+
+        public static RenderWindow gameWindow;
+
+        static void Main(string[] args)
+        {
+            gameWindow = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Youtube 2D MMORPG");
+            Game();
+        }
+
+        static void Game()
+        {
+
+            while (gameWindow.IsOpen)
+            {
+                gameWindow.DispatchEvents();
+                gameWindow.Clear(Color.Red);
+                gameWindow.Display();
+            }
+        }
+    }
+}
